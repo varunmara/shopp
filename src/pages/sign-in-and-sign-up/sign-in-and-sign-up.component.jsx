9fa045c3eecd9ff2404a9596225fb3327fd39229
varun mara <varungowde@gmail.com>
2019-11-11 23:47:57 -0600

    sign-in form input

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import "./sign-in-and-sign-up.styles.scss";
 
+import SignIn from "../../components/sign-in/sign-in.component";
+
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">Sign In</div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUpPage;
