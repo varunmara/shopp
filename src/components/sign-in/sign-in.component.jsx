c9f0359d5fafe8e34f635e3dd4b7a5a40c26702f
varun mara <varungowde@gmail.com>
2019-11-13 23:30:03 -0600

    added custom button component and integrated with the sign in page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -48,7 +49,7 @@ class SignIn extends React.Component {
             required
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit"> Sign in</CustomButton>
         </form>
       </div>
     );
